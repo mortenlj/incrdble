e5304cf928f390ccd6de6e19be37ec9e42985d94
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-11-25 19:29:38 +0100

    Create package for models

--- incrdble/web/__init__.py
+++ incrdble/web/__init__.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from lightkube import AsyncClient
 from lightkube.resources.apiextensions_v1 import CustomResourceDefinition
 
-from incrdble import models
+from incrdble.models import k8s
 from incrdble.deps import kubeclient
 
 router = APIRouter()
@@ -15,12 +15,12 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request, client: AsyncClient = Depends(kubeclient)):
-    crds = [models.BasicCrd.model_validate(crd.spec) async for crd in client.list(CustomResourceDefinition)]
+    crds = [k8s.BasicCrd.model_validate(crd.spec) async for crd in client.list(CustomResourceDefinition)]
     return templates.TemplateResponse(request=request, name="index.html.j2", context={"crds": crds})
 
 
 @router.get("/crd/{name}", response_class=HTMLResponse)
 async def get_crd(request: Request, name: str, client: AsyncClient = Depends(kubeclient)):
     kube_crd = await client.get(CustomResourceDefinition, name)
-    crd = models.Crd.model_validate(kube_crd.spec)
+    crd = k8s.Crd.model_validate(kube_crd.spec)
     return templates.TemplateResponse(request=request, name="crd.html.j2", context={"crd": crd})
