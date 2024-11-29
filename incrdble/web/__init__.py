d72a8352db3b38aa2c7386858cf86d1263bd5ed9
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-11-29 15:10:17 +0100

    Refactor to single form of modules again

--- incrdble/web/__init__.py
+++ incrdble/web/__init__.py
@@ -6,7 +6,7 @@ from lightkube import AsyncClient
 from lightkube.resources.apiextensions_v1 import CustomResourceDefinition
 
 from incrdble.deps import kubeclient
-from incrdble.models import k8s
+from incrdble import models
 
 router = APIRouter()
 
@@ -15,12 +15,12 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request, client: AsyncClient = Depends(kubeclient)):
-    crds = [k8s.BasicCrd.model_validate(crd.spec) async for crd in client.list(CustomResourceDefinition)]
+    crds = [models.BasicCrd.model_validate(crd.spec) async for crd in client.list(CustomResourceDefinition)]
     return templates.TemplateResponse(request=request, name="index.html.j2", context={"crds": crds})
 
 
 @router.get("/crd/{name}", response_class=HTMLResponse)
 async def get_crd(request: Request, name: str, client: AsyncClient = Depends(kubeclient)):
     kube_crd = await client.get(CustomResourceDefinition, name)
-    crd = k8s.Crd.model_validate(kube_crd.spec)
+    crd = models.Crd.model_validate(kube_crd.spec)
     return templates.TemplateResponse(request=request, name="crd.html.j2", context={"crd": crd})
